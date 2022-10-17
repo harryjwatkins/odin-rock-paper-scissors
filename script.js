dde711376a102fb824616356648028abd8d4e865
Harry Watkins <harry.jw@outlook.com>
2022-10-17 11:13:17 +0100

    Add function getComputerChoice and link script.js to index.html

--- script.js
+++ script.js
@@ -2,4 +2,13 @@
 
     "use strict"
 
+    function getComputerChoice(){
+        const choices = ["rock","paper","scissors"];
+        // Generate a random number between 0-2
+        const randomIndex = Math.floor(Math.random()*choices.length);
+
+        return choices[randomIndex];
+    }
+
+
 })()
